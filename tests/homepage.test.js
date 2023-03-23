9182b8e93dbfa0eac5abb1d5469c891cebcf33d8
lucic <lds8@njit.edu>
2023-03-23 13:25:11 -0400

    added the accept cookies test

--- tests/homepage.test.js
+++ tests/homepage.test.js
@@ -1,7 +1,15 @@
 const { test, expect } = require('@playwright/test');
 
-test('Homepage loads successfully', async ({ page }) => {
-  await page.goto('https://www.example.com/');
-  const pageTitle = await page.title();
-  expect(pageTitle).toBe('Example Domain');
+test('Accepting cookie closes modal', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  const modal = await page.$('.cookie-modal');
+  if (modal) {
+    const acceptButton = await modal.$('button.accept');
+    if (acceptButton) {
+      await acceptButton.click();
+      await page.waitForSelector('.cookie-modal', { state: 'hidden' });
+      const modalAfterAccept = await page.$('.cookie-modal');
+      expect(modalAfterAccept).toBeNull();
+    }
+  }
 });
