23470e38d8b9a6c65c82d1f67ef152408c73d874
lucic <lds8@njit.edu>
2023-03-23 11:41:25 -0400

    modified alt-text

--- tests/alt-text.test.js
+++ tests/alt-text.test.js
@@ -2,7 +2,7 @@ const { chromium } = require('@playwright/test');
 
 (async () => {
   const browser = await chromium.launch();
-  const context = await browser.newContext();
+  const context = await browser.newContext();us
   const page = await context.newPage();
 
   await page.goto('http://localhost:3000');
